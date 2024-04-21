fcfbf4fbc587495c59bc641a01ea7c517777a823
Biswojit Beja <149652501+Biswojit77@users.noreply.github.com>
2024-04-21 18:51:46 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const acoountId = 210301
+let accountEmail ="biswojit@gmail.com"
+var accountCity = "Bhubaneswar"
+accountcity = "khorda"
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountCity = "Bhubaneswar"
+accountEmail ="abc@gmail.com"
+
+
+console.log(accountEmail);
+
+/*
+Prefer not to use var because of issue in block scope and funcional scope
+*/
+
+
+console.table([accountCity,accountEmail,acoountId,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all code in newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(
+    3+  
+    3) //code readability should be high
+
+
+console.log("Biswojit")
+
+let name ="biswojit"
+let age = 20
+let isLoggedIn = true
+
+
+//primitive dataTypes
+//number => 2 to the power 53
+//bigint =>
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique 
+
+//Object
+
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
