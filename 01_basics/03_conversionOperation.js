90206c110dee0ec0f53685977ac1b3e4cf540557
Biswojit Beja <149652501+Biswojit77@users.noreply.github.com>
2024-04-22 07:02:08 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Biswo"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33"=>33
+//"33biswo"=>NaN
+// true=>1;
+//false=>0;
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn )
+
+
+//1=>true; 0=>false
+//""=>false
+//"biswo"=> true
+
+let someNumber = 12
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
