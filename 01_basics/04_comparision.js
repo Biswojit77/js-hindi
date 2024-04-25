b8145ed789de8bd2dbd7e4c9c90a6628f0d1cc25
Biswojit Beja <149652501+Biswojit77@users.noreply.github.com>
2024-04-25 17:15:13 +0000

    conversion & comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2" > 1)
+console.log("02" > 1);
+
+// console.log(null > 0) its confusion
+// console.log(null == 0) its confusion
+// console.log(null >= 0) its confusion
+
+// console.log(undefined == 0) its confusion
+// console.log(undefined > 0) its confusion
+// console.log(undefined < 0) its confusion
+
+
+// ===(strictly check)
+
+console.log("2" === 2)
