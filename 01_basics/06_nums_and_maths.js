3f86195e4b325e592cc8edf5f549c855f29b30d9
Biswojit Beja <149652501+Biswojit77@users.noreply.github.com>
2024-04-27 05:39:28 +0000

    Nums And Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score =400
+
+const balance = new Number (100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const otherNmae = 87.282
+
+console.log(otherNmae.toPrecision(3))
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++maths++++++++++++++++++++//
+
+
+console.log(Math)
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.3));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(7.4));
+
+console.log(Math.min(1,2,12,12,2,3,5,32,2,23,23));
+console.log(Math.max(323,23323,23,23,23,32,32,3,23,23,2323,23,2323));
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)));
+
+
